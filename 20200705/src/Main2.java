b2f87ec59c28d3c28918df58ce889369d3057124
LHJ-123 <15529867019@163.com>
2020-07-05 22:54:42 +0800

    day30

---
+++ 20200705/src/Main2.java
@@ -0,0 +1,21 @@
+//day30 2
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            BigInteger[] bigIntegers = new BigInteger[n];
+            for(int i = 0;i<n;i++) {
+                bigIntegers[i] = scanner.nextBigInteger();
+            }
+            Arrays.sort(bigIntegers);
+            for(int i = 0;i<n;i++) {
+                System.out.println(bigIntegers[i]);
+            }
+        }
+    }
+}
