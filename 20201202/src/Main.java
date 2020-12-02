22cd9f11df31fe9568f98155843428a0961974e6
LHJ-123 <15529867019@163.com>
2020-12-02 21:49:14 +0800

    机器人走方格

---
+++ 20201202/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+abstract public class Main {
+    int a;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+        int a = 12;
+        System.out.println(a+b);
+
+    }
+}
